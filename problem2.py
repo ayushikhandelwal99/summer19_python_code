c13958d94bca136f2f0a1896eda63a8bdb5efb17
Ayushi Khandelwal <ayushikhandelwal9919@yahoo.com>
2019-06-11 23:38:17 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 
-
 from googlesearch import search
-web=input("Please enter topic:")
+import speech_recognition
 
-url=[]
-for i in search(web,stop=10):
-        url.append(i)
-
-
-f=open('file.txt','a+')
-for i in url:
-	f.write(i+'\n')
-f.seek(0)
-print(f.read())
-f.close()
-
+options = '''
+Press 1 to type topic
+Press 2 to for mic
+'''
+print(option)
+choice = input("Enter your choice:")
+url =[]
+if choice == '1' :
+	web=input("Please enter topic:")
+	for i in search(web,stop=10):
+        	url.append(i)
+        f=open('urls.txt','a+')
+	for i in url:
+		f.write(i+'\n')
+	f.seek(0)
+	print(f.read())
+	f.close()
+elif choice == '2' :
+	print("program still in process")
+else :
+	print("Enter correct choice!!!!")
 
