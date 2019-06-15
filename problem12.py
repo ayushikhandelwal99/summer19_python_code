0c577928b87e0d2510416ce10f1eb3f06f19692c
Ayushi Khandelwal <ayushikhandelwal9919@yahoo.com>
2019-06-15 16:32:24 +0000

    QRcode of your search
    write a python code to do the following
    
    i)  take input from user and search in google
    ii)   pick the first 3 url of google search
    iii)  make  QR-code of all 3 url's
    iv)   Store all these qr-code in  apache web server in aws cloud

--- problem12.py
+++ problem12.py
@@ -7,12 +7,18 @@ from googlesearch import search
 urlist=[]
 u=0
 web=input("Enter your choice:  ")
-for i in search(web,stop=5):
+for i in search(web,stop=3):
 	urlist.append(i)
 	print(i)
 
 	url = pyqrcode.create(urlist[u])
 	url.svg("qr"+str(u)+".svg", scale = 6)
 	u+=1
-	print(url.terminal())
-os.system('mv *.png /var/www/html')
+file=input("enter name of .html file: ")
+
+os.system('touch'+' '+file)
+os.system('echo "<html><h1>QR code for your search:n </h1><body><img src="1.svg"><img src="2.svg"><img src="3.svg"></body></html>"'+file)
+os.system('sudo mv '+file+' '+'/var/www/html/)
+os.system('sudo mv 1.svg /var/www/html/)
+os.system('sudo mv 2.svg /var/www/html/)
+os.system('sudo mv 3.svg /var/www/html/)
